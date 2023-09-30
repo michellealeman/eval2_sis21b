beda090440fa38ca090b3830620cfe996c595075
michellealeman <143537167+michellealeman@users.noreply.github.com>
2023-09-30 13:10:40 -0600

    about

---
+++ lib/views/about_us.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+class AboutUs extends StatelessWidget {
+// Datos de ambos
+final String names = 'Kenia Chavarria\nMario Lopez'; // Nombres de ambos
+final String emails =
+'georgina.aleman22.itca.edu.sv\tania. juarez22@itca.edu.sv'; // Correos 
+//de ambos
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+appBar: AppBar(
+title: Text('Acerca de nosotros'),
+),
+body: Center(
+child: Column(
+mainAxisAlignment: MainAxisAlignment.center,
+children: <Widget>[
+// Imagen con ambas fotos
+Image.asset(
+'images/equipo.jpg', // Ruta de la imagen combinada
+width: 150.0,
+height: 150.0,
+),
+SizedBox(height: 20.0),
+Text(
+names,
+textAlign: TextAlign.center,
+style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+),
+SizedBox(height: 10.0),
+Text(
+emails,
+textAlign: TextAlign.center,
+style: TextStyle(fontSize: 18.0),
+),
+],
+),
+),
+);
+}
+}
